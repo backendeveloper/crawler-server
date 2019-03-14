46e544b66f72754feb405fe0a6a08483d20a8596
Kaan Uygur <kaan@evtiko.com>
2019-03-14 10:41:29 +0300

    add example.com

--- server.js
+++ server.js
@@ -40,6 +40,42 @@ let scrape = async (path) => {
     return result;
 };
 
+let scrapeGoogle = async (path) => {
+    const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox'] });
+    const page = await browser.newPage();
+
+    await page.goto(path);
+
+    const result = await page.evaluate(() => {
+        return {
+            width: document.documentElement.clientWidth,
+            height: document.documentElement.clientHeight,
+            deviceScaleFactor: window.devicePixelRatio
+        };
+    });
+
+    // const result = await page.evaluate(() => {
+    //     const featureArticle = document
+    //         .evaluate(
+    //             '//*[@id="classifiedDetail"]/div[1]/div[1]/h1',
+    //             document,
+    //             null,
+    //             XPathResult.FIRST_ORDERED_NODE_TYPE,
+    //             null
+    //         )
+    //         .singleNodeValue;
+
+    //     if (featureArticle == null) {
+    //         return 'Blocked!';
+    //     } else {
+    //         return featureArticle.textContent;
+    //     }
+    // });
+
+    browser.close();
+    return result;
+};
+
 app.get('/getpath', (req, res) => {
     let url = req.query.url;
 
@@ -52,10 +88,28 @@ app.get('/getpath', (req, res) => {
         console.log('value: ', value);
         // console.log('requestCounter :', counter);
         // console.log('blockedrequestCounter :', blockedCounter);
-        
+
         // return value;
         res.status(200).json(value);
-    });    
+    });
+});
+
+app.get('/getgooglepath', (req, res) => {
+    let url = req.query.url;
+
+    scrapeGoogle(url).then((value) => {
+        // if (value == 'Blocked!') {
+        //     blockedCounter++;
+        // } else {
+        //     counter++;
+        // }
+        console.log('value: ', value);
+        // console.log('requestCounter :', counter);
+        // console.log('blockedrequestCounter :', blockedCounter);
+
+        // return value;
+        res.status(200).json(value);
+    });
 });
 
 app.get('/check', (req, res) => {
